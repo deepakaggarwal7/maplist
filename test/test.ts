c7a60872a96bd416517c4ee58fcc00203cd6b7d2
Deepak Aggarwal <deepakaggarwal7@gmail.com>
2024-07-25 20:04:57 +0530

    create from method added

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
 import {MapList, RecordUtil} from '../dist'
+const {filter,find, findAll,map , createFrom} = RecordUtil
 import assert  from 'assert'
 
 interface IPost{
@@ -21,28 +22,44 @@ const dataFromSomeAPI:IUser[] = [
 
 
 //Positive
-const map = MapList.createFrom(dataFromSomeAPI);
-assert.equal(map.size,4)
+const keyedData = MapList.createFrom(dataFromSomeAPI);
+assert.equal(keyedData.size,4)
 
-const mapSearchByKeys = map.findAll([101,103])
+const mapSearchByKeys = keyedData.findAll([101,103])
 assert.equal(mapSearchByKeys.length,2)
 
 
-const searchedOne = map.find(x=>x.name == "D")
+const searchedOne = keyedData.find(x=>x.name == "D")
 assert.equal(searchedOne!.id,103)
 
-const filtered = map.filter(m=>m.posts.some(p=>p.isPublished))
+const filtered = keyedData.filter(m=>m.posts.some(p=>p.isPublished))
 assert.equal(filtered.length,1)
 assert.equal(filtered[0].id,102)
 
 
 //Negatives
-const mapSearchByKeysNegative = map.findAll([109,110])
+const mapSearchByKeysNegative = keyedData.findAll([109,110])
 assert.equal(mapSearchByKeysNegative.length,0)
 
-const searchedOneNegative = map.find(x=>x.name == "E")
+const searchedOneNegative = keyedData.find(x=>x.name == "E")
 assert.equal(searchedOneNegative,undefined)
 
-const filteredNegative = map.filter(m=>m.posts.length>5)
+const filteredNegative = keyedData.filter(m=>m.posts.length>5)
 assert.equal(filteredNegative.length,0)
 
+
+interface ICar {
+    tyre:number
+}
+
+
+// const listData = [{id:1, tyre:11}, {id:2, tyre:22}, {id:3, tyre:33}]
+// const data7 = createFrom(listData,"id")
+
+// const data: Record<number, ICar> = {1:{tyre:11}, 2:{tyre:22}, 3:{tyre:33}, 4:{tyre:44}}
+
+// const filtered = filter(data, x=>x.tyre > 20)
+
+// const finded = findAll(data, [1,2,3])
+
+// const mapped = keyedData(data, r=>(r.tyre + 10))
