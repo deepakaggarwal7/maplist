ecb6c9e1555c08afbc4a7c9e6e5159ae4c3aba9a
Deepak Aggarwal <deepakaggarwal7@gmail.com>
2024-07-30 12:27:39 +0530

    forEach  method added

--- src/MapList.ts
+++ src/MapList.ts
@@ -1,69 +1,74 @@
 export class MapList<K extends string | number, V> extends Map<K, V> {
-    constructor(entries?: [K, V][]) {
-      super(entries);
-    }
-  
-    find(predicate: (value: V) => boolean): V | undefined {
-      for (const value of this.values()) {
-        if (predicate(value)) {
-          return value;
-        }
-      }
-      return undefined;
-    }
-  
-    findAll(keys: K[]): V[] {
-      const results: V[] = [];
-      for (const key of keys) {
-        if (this.has(key)) {
-          results.push(this.get(key) as V);
-        }
-      }
-      return results;
-    }
-  
-    filter(predicate: (value: V) => boolean): V[] {
-      const results: V[] = [];
-      for (const value of this.values()) {
-        if (predicate(value)) {
-          results.push(value);
-        }
-      }
-      return results;
-    }
+  constructor(entries?: [K, V][]) {
+    super(entries);
+  }
 
-    some(predicate: (value: V) => boolean): boolean {
-      for (const value of this.values()) {
-        if (predicate(value)) {
-          return true;
-        }
+  find(predicate: (value: V) => boolean): V | undefined {
+    for (const value of this.values()) {
+      if (predicate(value)) {
+        return value;
       }
-      return false;
     }
-  
-    map<T>(callback: (value: V) => T): T[] {
-      const results: T[] = [];
-      for (const value of this.values()) {
-        results.push(callback(value));
+    return undefined;
+  }
+
+  findAll(keys: K[]): V[] {
+    const results: V[] = [];
+    for (const key of keys) {
+      if (this.has(key)) {
+        results.push(this.get(key) as V);
       }
-      return results;
     }
+    return results;
+  }
 
-
-    static createFrom<T extends { id?: any }, K extends keyof T>(
-        list: T[],
-        keyProperty?: K
-      ): MapList<Extract<T[K], string | number>, T> {
-        const mapEntries: [Extract<T[K], string | number>, T][] = [];
-  
-      for (const item of list) {
-        const key = keyProperty ? item[keyProperty] : item['id'];
-        if (key === undefined) {
-          throw new Error("Key property not found on item");
-        }
-        mapEntries.push([key, item]);
+  filter(predicate: (value: V) => boolean): V[] {
+    const results: V[] = [];
+    for (const value of this.values()) {
+      if (predicate(value)) {
+        results.push(value);
       }
-  
-      return new MapList(mapEntries);
+    }
+    return results;
+  }
+
+  some(predicate: (value: V) => boolean): boolean {
+    for (const value of this.values()) {
+      if (predicate(value)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  map<T>(callback: (value: V) => T): T[] {
+    const results: T[] = [];
+    for (const value of this.values()) {
+      results.push(callback(value));
+    }
+    return results;
+  }
+
+  foreach(callback: (value: V) => void) {
+    for (const value of this.values()) {
+      callback(value);
     }
   }
+
+  static createFrom<T extends { id?: any }, K extends keyof T>(
+    list: T[],
+    keyProperty?: K
+  ): MapList<Extract<T[K], string | number>, T> {
+    const mapEntries: [Extract<T[K], string | number>, T][] = [];
+
+    for (const item of list) {
+      const key = keyProperty ? item[keyProperty] : item["id"];
+      if (key === undefined) {
+        throw new Error("Key property not found on item");
+      }
+      mapEntries.push([key, item]);
+    }
+
+    return new MapList(mapEntries);
+  }
+}
